85a57b8cfc3d2842f99fb2d8f71931914bc6688c
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2021-12-07 17:55:10 +0530

    Time: 2 ms (17.29%), Space: 6 MB (70.61%) - LeetHub

--- integer-break/integer-break.cpp
+++ integer-break/integer-break.cpp
@@ -1,26 +1,23 @@
 
-int dp[60];
+// int dp[60];
 
 class Solution {
-public:
-    
-    
-    
-    int solver(int n)
-    {
-        if(n==1)
-            return 1;
+public:    
+//    int solver(int n)
+//     {
+//         if(n==1)
+//             return 1;
         
-        if(dp[n]!=-1)
-            return dp[n];
-        int i,ans=n;
+//         if(dp[n]!=-1)
+//             return dp[n];
+//         int i,ans=n;
         
-        for(i=1;i<n;i++)
-        {
-            ans = max(ans,solver(i)*solver(n-i));
-        }
-       return dp[n]=ans;
-    }
+//         for(i=1;i<n;i++)
+//         {
+//             ans = max(ans,solver(i)*solver(n-i));
+//         }
+//        return dp[n]=ans;
+//     }
     
     int integerBreak(int n) {
         
@@ -29,8 +26,27 @@ public:
         if(n<=3)
             return n-1;
         
-        memset(dp,-1,sizeof dp);   
-        return solver(n);
+         int dp[n+1];
+        
+        memset(dp,-1,sizeof dp);
+        
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        
+        for(int i=4;i<=n;i++)
+        {
+            dp[i]=i;
+            for(int j=1;j<i;j++)
+            {
+               dp[i] = max(dp[i],dp[j]*dp[i-j]);
+            }
+           
+        }
+        
+        // memset(dp,-1,sizeof dp);   
+        return dp[n];
         
         
     }
