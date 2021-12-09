a8f669f70282d046aafc513861148ae0c813d5e3
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2021-12-09 14:26:29 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (85.19%) - LeetHub

--- unique-paths-ii/unique-paths-ii.cpp
+++ unique-paths-ii/unique-paths-ii.cpp
@@ -1,46 +1,77 @@
 
-int dp[105][105];
+unsigned int dp[105][105];
 class Solution {
 public:
     
-     bool isValid(int i,int j,int n, int m,vector<vector<int>>& grid)
-    {
-        if(i<0||i>=n||j<0||j>=m||grid[i][j]==1)
-            return false;
-        return true;
-    }
+//      bool isValid(int i,int j,int n, int m,vector<vector<int>>& grid)
+//     {
+//         if(i<0||i>=n||j<0||j>=m||grid[i][j]==1)
+//             return false;
+//         return true;
+//     }
     
-    int solver(int i,int j,int n,int m,vector<vector<int>>& grid)
-    {
+//     int solver(int i,int j,int n,int m,vector<vector<int>>& grid)
+//     {
         
-        if(!isValid(i,j,n,m,grid))
-        {
-            return 0;
-        }
+//         if(!isValid(i,j,n,m,grid))
+//         {
+//             return 0;
+//         }
         
-        if(i==n-1&&j==m-1)
-        {
-            // cout<<"here";
-            return 1;
-        }
+//         if(i==n-1&&j==m-1)
+//         {
+//             // cout<<"here";
+//             return 1;
+//         }
         
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
+//         if(dp[i][j]!=-1)
+//         {
+//             return dp[i][j];
+//         }
         
-        int temp = solver(i+1,j,n,m,grid)+solver(i,j+1,n,m,grid);
+//         int temp = solver(i+1,j,n,m,grid)+solver(i,j+1,n,m,grid);
         
-        return dp[i][j]=temp;
-    }
+//         return dp[i][j]=temp;
+//     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         
         int n = grid.size();
         int m = grid[0].size();
         
-        memset(dp,-1,sizeof dp);
+//         memset(dp,-1,sizeof dp);
         
-        return solver(0,0,n,m,grid);
+//         return solver(0,0,n,m,grid);
+        
+         memset(dp,0,sizeof dp);
+        
+        
+        for(int i=m-1;i>=0;i--)
+        {
+            if(grid[n-1][i]!=1)
+            dp[n-1][i]=1;
+            else
+                break;
+                
+        }
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(grid[i][m-1]!=1)
+            dp[i][m-1]=1;
+            else
+                break;
+        }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=m-2;j>=0;j--)
+            {
+                if(grid[i][j]!=1)
+                dp[i][j]=(dp[i+1][j]+dp[i][j+1]);
+            }
+        }
+                
+        return dp[0][0];
         
     }
 };
