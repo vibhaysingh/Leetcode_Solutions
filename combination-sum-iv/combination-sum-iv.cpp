491f58e35be3fb1f3c95972ed89956f49a60f3a2
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2021-12-09 10:29:26 +0530

    Time: 3 ms (42.05%), Space: 7.4 MB (5.26%) - LeetHub

--- combination-sum-iv/combination-sum-iv.cpp
+++ combination-sum-iv/combination-sum-iv.cpp
@@ -1,27 +1,64 @@
-unsigned int dp[1005];
+unsigned int dp[205][1005];
 
 class Solution {
 public:
+    
+    
+    int solver(vector<int>& coins, int amount,int idx)
+    {
+        if(idx>=coins.size())
+            return 0;
+        if(amount==0)
+        {
+            return 1;
+        }
+        
+        if(dp[idx][amount]!=-1)
+        {
+            return dp[idx][amount];
+        }
+        
+        int temp=0;
+        
+        for(int i=idx;i<coins.size();i++)
+        {
+            if(coins[i]<=amount)
+            {
+                temp+=solver(coins,amount-coins[i],idx)+solver(coins,amount,idx-1);
+            }
+            else
+            {
+                temp+=solver(coins,amount,idx-1);
+            }
+        }
+        
+        return dp[idx][amount]=temp;
+    }
+    
+    
     int combinationSum4(vector<int>& coins, int amount) {
         
-        memset(dp,0,sizeof dp);        
+        memset(dp,-1,sizeof dp);        
         // vector<int>dp(amount+1,0);
-        int l = coins.size();
         
-        dp[0]=1;
+//         int l = coins.size();
         
-         for(int i=1;i<amount+1;i++)
-         {
-             for(int j=0;j<l;j++)
-             {
-                 if(coins[j]<=i)
-                 {
-                     dp[i]+=dp[i-coins[j]];
-                 }
-             }
-         }
+//         dp[0]=1;
+        
+//          for(int i=1;i<amount+1;i++)
+//          {
+//              for(int j=0;j<l;j++)
+//              {
+//                  if(coins[j]<=i)
+//                  {
+//                      dp[i]+=dp[i-coins[j]];
+//                  }
+//              }
+//          }
         
         
-        return dp[amount];
+//         return dp[amount];
+        
+        return solver(coins,amount,0);
     }
 };
