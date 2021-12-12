c197a628009d21d27a33ea9d9404ba5d4a688222
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2021-12-12 10:24:15 +0530

    Time: 99 ms (31.55%), Space: 48.3 MB (78.53%) - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -4,35 +4,55 @@ int dp[10005];
 class Solution {
 public:
     
-    bool solver(int idx, vector<int>&nums)
-    {
-        if(idx>=nums.size())
-            return false;
-        if(idx==nums.size()-1)
-        {
-            return true;
-        }
+//     bool solver(int idx, vector<int>&nums)
+//     {
+//         if(idx>=nums.size())
+//             return false;
+//         if(idx==nums.size()-1)
+//         {
+//             return true;
+//         }
         
-        if(dp[idx]!=-1)
-        {
-            return dp[idx];
-        }
+//         if(dp[idx]!=-1)
+//         {
+//             return dp[idx];
+//         }
     
         
-        for(int i=1;i<=nums[idx];i++)
-        {
-            if(solver(idx+i,nums))
-            {
-                return dp[idx+i]=true;
-            }
-        }
-        return dp[idx]=false;
-    }
+//         for(int i=1;i<=nums[idx];i++)
+//         {
+//             if(solver(idx+i,nums))
+//             {
+//                 return dp[idx+i]=true;
+//             }
+//         }
+//         return dp[idx]=false;
+//     }
     
     bool canJump(vector<int>& nums) {
         
-        memset(dp,-1,sizeof dp);
+        // memset(dp,-1,sizeof dp);        
+        // return solver(0,nums);
         
-        return solver(0,nums);
+        memset(dp,0,sizeof dp);  
+        
+        dp[0]=nums[0];
+        int n = nums.size();
+        
+        if(n==1)return true;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(dp[i-1]<i)
+                return false;
+            dp[i]= max(dp[i-1],i+nums[i]);
+            
+            if(dp[i]>=n-1)
+                return true;
+        }
+        
+        return false;
+        
+        
     }
 };
