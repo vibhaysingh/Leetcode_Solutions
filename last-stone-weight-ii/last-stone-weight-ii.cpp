2e73b763a817b86f540c1ff17be52c8a19b1b59c
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2021-12-14 15:31:21 +0530

    Time: 4 ms (80.62%), Space: 8.4 MB (48.06%) - LeetHub

---
+++ last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -0,0 +1,64 @@
+
+int dp[35][3005];
+
+class Solution {
+public:    
+        // a< b < c < d
+        // 1 4 6 9
+        // a c-b d
+        // 1 6-4 9  -->1 2 9
+        // a (d-(c-b))--> a, d, -c, b
+        // 1 (9-(6-4))--> 1 ,7
+        // (d-(c-b)) - a --> d, -c, b , -a 
+        // (9-(6-4))-1 --> 6    
+        // simlar of putting +/- infront of every number and take minimum subset sum
+    
+    //   1 2 3
+    //   1 2 3
+    
+    int lastStoneWeightII(vector<int>& st) {
+        
+        int n = st.size();
+        int sum = accumulate(st.begin(),st.end(),0);
+        
+        
+            
+        
+           memset(dp,-1,sizeof dp);
+        
+        for (int i = 0; i <= n; i++)
+          dp[i][0] = 1;
+ 
+      for (int i = 1; i <= sum; i++)
+            dp[0][i] = 0;
+            
+        
+        for (int i = 1; i <= n; i++) 
+        {
+        for (int j = 1; j <= sum; j++) 
+        {
+            // If i'th element is excluded
+            dp[i][j] = dp[i - 1][j];
+ 
+            // If i'th element is included
+            if (st[i - 1] <= j)
+                dp[i][j] |= dp[i - 1][j - st[i - 1]];
+        }
+       }
+        
+        int diff= INT_MAX;
+        
+        for (int j = sum / 2; j >= 0; j--) 
+        {
+
+        if (dp[n][j] == 1) {
+            diff = sum - 2 * j;
+            break;
+        }
+        }
+        
+        return diff;
+            
+            
+    }
+};
