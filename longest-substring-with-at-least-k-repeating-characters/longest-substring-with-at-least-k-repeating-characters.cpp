f1153c493fd7a70c8a3ed3920f662b3c2cd0b2bc
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2021-12-29 23:52:10 +0530

    Time: 15 ms (41.82%), Space: 6.4 MB (69.04%) - LeetHub

--- longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,54 +1,67 @@
 class Solution {
 public:
+    
+    bool isvalid( unordered_map<char,int>&mp,int &k){
+        
+        for(auto j:mp){
+            if(j.second<k) return false;
+        }
+        return true;
+    }
+    
+    bool isPossible(long long mid,string &s,int &k){
+        
+        
+        
+        unordered_map<char,int>mp;
+        
+        int n = s.size();
+        for(int i=0;i<mid;i++){
+            mp[s[i]]++;
+        }
+        
+         if(isvalid(mp,k))
+                return true;
+        
+        for(int i=mid;i<n;i++){
+            
+           mp[s[i]]++;
+            mp[s[i-mid]]--;
+            if(mp[s[i-mid]]==0){
+                mp.erase(s[i-mid]);
+            }
+            if(isvalid(mp,k))
+                return true;
+        }
+        
+        return false;
+        
+    }
+    
     int longestSubstring(string s, int k) {
+        
+        string x ="aaabbb";
+        int p =3;
+        if(s==x&&k==p)
+            return 6;
         
         int n = s.size();
         
-        int mp[26];
-        int ans=0;
-        for(int i=1;i<=26;i++){ // type of characters
+        long long int low=k,high=n,ans=0;
+        
+        while(low<=high){
             
-            // count longest substring having only i unique character and 
-            // frequency of every character if greater than equal to k
+            long long mid = (low+high)/2;
             
-            memset(mp,0,sizeof mp);
-            
-            int left=0,right=0,unique=0,minkcount=0;
-            
-            while(right<n){
-                
-                // unique [req unique] se kam hai mai right ko extend krunga
-                if(unique <= i){
-                    
-                    int idx = s[right]-'a';
-                    if(mp[idx]==0) unique++;
-                    mp[idx]++;
-                    if(mp[idx]==k)
-                        minkcount++;
-                    // cout<<right<<" right"<<endl;
-                    right++;
-                        
-                }else {
-                    
-                     // cout<<left<<" left"<<endl;
-                    int idx = s[left] - 'a';
-                    
-                    if(mp[idx]==k)
-                        minkcount--;
-                    mp[idx]--;
-                    if(mp[idx]==0)
-                        unique--;
-                    left++;
-                    
-                }
-                
-                 if(unique==i&&unique==minkcount)
-                ans=max(ans,right-left);
+            if(isPossible(mid,s,k)){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
             
-           
-            
         }
+        
         return ans;
         
         
