b2fb70033deb126e7a29e141088f62df0ec0fa2e
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2021-12-29 13:54:16 +0530

    Time: 119 ms (6.83%), Space: 61.6 MB (26.40%) - LeetHub

--- increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
+
+     // Time O(n logn)  space O(n) 
         
-        int n = nums.size();
+        int n = nums.size();        
+//         vector<int>ans;
+//         ans.push_back(nums[0]);
         
-        vector<int>ans;
-        ans.push_back(nums[0]);
-        
-        for(int i=1;i<n;i++){
+//         for(int i=1;i<n;i++){
             
-            if(ans.back()>=nums[i]){
+//             if(ans.back()>=nums[i]){
                 
-                auto k = lower_bound(ans.begin(),ans.end(),nums[i]);
-                *k=nums[i];
+//                 auto k = lower_bound(ans.begin(),ans.end(),nums[i]);
+//                 *k=nums[i];
                 
-            }else{
-                ans.push_back(nums[i]);
+//             }else{
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+        
+//         return ans.size()>=3;
+        
+        int first=INT_MAX,second=INT_MAX;
+        
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]<=first){
+                first=nums[i];
+            }
+            else if(nums[i]<=second){
+                second=nums[i];
+            }
+            else{
+                // cout<<nums[i]<<endl;
+                return true;
             }
         }
         
-        return ans.size()>=3;
+        return false;
         
         
     }
 };
 
-/// 1 2 8 4
+/// 1 2 -1 4
