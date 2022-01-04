37c12e9e27ee3553bbc7d313f87e59e60f254201
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-04 10:12:00 +0530

    Time: 28 ms (15.63%), Space: 19.7 MB (49.56%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-        int candidate =nums[0];
-        int votes =1;
-        
-        for(int i=1;i<nums.size();i++){
+  
+        // M-1 Voting Algo
+//         int candidate =nums[0];
+//         int votes =1;        
+//         for(int i=1;i<nums.size();i++){
             
-            if(votes==0){
-                candidate =nums[i];
-                votes=1;
+//             if(votes==0){
+//                 candidate =nums[i];
+//                 votes=1;
+//             }
+//             else if(candidate==nums[i]){
+//                 votes++;
+//             }else if(candidate!=nums[i]){
+//                 votes--;
+//             }
+//         }      
+//             return candidate;
+        
+        // M-2 Bit Manipulation
+        
+        int candidate =0;
+        int n = nums.size();
+        
+        for(int i=0;i<32;i++){
+            
+            int count=0; // count of ith bit in every number
+            
+            for(int j=0;j<n;j++){
+                
+                if((nums[j]>>i)&1)
+                    count++;
             }
-            else if(candidate==nums[i]){
-                votes++;
-            }else if(candidate!=nums[i]){
-                votes--;
-            }
+            
+            if(count>n/2)
+                candidate=(candidate |(1<<i));
+            
+            
         }
         
-        
-        
-        
-            return candidate;
+        return candidate;
         
         
     }
