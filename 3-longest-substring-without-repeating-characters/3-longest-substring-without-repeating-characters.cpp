125158cd5e405e6567112d11a606f75ad90310d2
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-04 15:56:22 +0530

    Time: 27 ms (36.75%), Space: 8.2 MB (69.75%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -19,22 +19,39 @@ public:
         int i=0, j=0;
         int ans=0;
         
-        while(j<n){
+//         while(j<n){
             
-            mp[s[j]]++;
+//             mp[s[j]]++;
             
-            while(!valid(mp)){
+//             while(!valid(mp)){
                 
-                mp[s[i]]--;
-                if(mp[s[i]]==0){
-                    mp.erase(s[i]);
-                }
-                i++;
+//                 mp[s[i]]--;
+//                 if(mp[s[i]]==0){
+//                     mp.erase(s[i]);
+//                 }
+//                 i++;
+//             }
+            
+//             ans = max(ans,j-i+1);
+//             j++;
+            
+//         }
+        
+        
+        while(i<n && j<n){
+            
+            
+            if(mp.count(s[i])==0){
+                mp[s[i]]=i;
+                ans = max(ans,i-j+1);
+            }else{
+                
+                int prevIdx = mp[s[i]];
+                j = max(j,prevIdx+1);
+               ans = max(ans,i-j+1);
+                mp[s[i]]=i;
             }
-            
-            ans = max(ans,j-i+1);
-            j++;
-            
+            i++;
         }
         
         return ans;
