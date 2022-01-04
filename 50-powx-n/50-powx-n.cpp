bead84b4bc2cb147b32c645a4e778d594130d09d
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-04 09:26:50 +0530

    Time: 3 ms (12.97%), Space: 5.9 MB (41.10%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -18,9 +18,27 @@ public:
     
     double myPow(double x, int n) {
         
-        double ans = solver(x,n);
-        if(n<0)
-            return 1/ans;
+        // double ans = solver(x,n);
+        // if(n<0)
+        //     return 1/ans;
+        // return ans;
+        
+        int nn =abs(n);
+        
+        double ans = 1.0;
+        
+        while(nn>0){
+            
+            if(nn%2){
+                ans=ans*x;               
+            }
+                x = x*x;
+                nn/=2;
+        }
+        
+        if(n<0)return (double)1/ans;
         return ans;
+        
+        
     }
 };
