4ca29f032bdae397957fa5fd8af70e372fa2afe7
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-08 09:58:34 +0530

    Time: 11 ms (33.33%), Space: 11.8 MB (9.14%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -16,53 +16,32 @@ public:
 
 class Solution {
 public:
+    
+    map<Node*,Node*>mp; // < old,new >
+    
+    Node* solver(Node* head){
+        
+        if(head==NULL){
+            return NULL;
+        }
+        
+        Node * new_head = new Node(head->val);
+        
+        mp[head]=new_head;
+        new_head->next = solver(head->next);
+        
+        if(head->random!=NULL){
+            
+            new_head->random = mp[head->random];
+        }
+        
+        return new_head;
+        
+    }
+    
     Node* copyRandomList(Node* head) {
         
-        if(head==NULL) return NULL;
-        
-        Node* curr = head;
-        Node* next = NULL;
-        
-        
-        
-        while(curr!=NULL){
-            
-            Node* copy = new Node(curr->val);            
-            next = curr->next;
-            curr->next = copy;
-            copy->next = next;
-            curr=next;            
-        }
-        
-        curr = head;
-        
-        // point all the random pointers
-        while(curr!=NULL){
-            
-            // if random is not null
-            if(curr->random)
-            curr->next->random = curr->random->next;
-            curr=curr->next->next;
-        }
-        
-        Node* ans  = head->next;
-        curr=head;
-        // restore the original list;
-        Node* temp;
-        while(curr!=NULL){
-            
-            next = curr->next;
-            curr->next = curr->next->next;            
-            temp =  next->next;  
-            if(next->next)
-            next->next = temp->next;
-            curr=temp;
-            
-        }
-        
-        return ans;
-        
-        
+       return solver(head); 
         
     }
 };
