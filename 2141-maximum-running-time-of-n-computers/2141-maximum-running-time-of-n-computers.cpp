a1acbfe602ad47ed266a8e9b0f9fcacafaf24145
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-16 15:19:11 +0530

    Time: 190 ms (60.00%), Space: 57.9 MB (6.67%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
     
-    
-    bool possible(vector<int>& batteries,long long mid,int n){
         
-        long long sum = 0;
-        
-        for(int i=0;i<batteries.size();i++){
-            
-            sum+=min((long long)batteries[i],mid);
-        }
-        
-        
-        return (sum>=(long long)n*mid);
-        
+        long long maxRunTime(int n, vector<int>& bat) {
+    long long sum = accumulate(begin(bat), end(bat), 0LL);
+    priority_queue<int> pq(begin(bat), end(bat));
+    while (pq.top() > sum / n) {
+        sum -= pq.top(); pq.pop();
+        --n;
     }
+    return sum / n;
+}
+        
     
-    long long maxRunTime(int n, vector<int>& batteries) {
-        
-        
-        long long low = 0,high=0,mid;
-        
-        for(auto k:batteries){
-            high+=k;
-        }
-        
-        long long ans = 0;
-        
-        while(low<=high){
-            
-            mid = (low+high)/2;
-            
-            if(possible(batteries,mid,n)){
-                ans = max(ans,mid);
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        
-        return ans;
-        
-    }
 };
