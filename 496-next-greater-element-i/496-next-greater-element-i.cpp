becdef6030eda07941f9c3b23b77a2cc092cfbee
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-21 10:48:48 +0530

    Time: 9 ms (38.11%), Space: 9.1 MB (15.67%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        int a[10005];
+        
+        memset(a,-1,sizeof a);
+        
+        stack<int>st;
+        
+        int n  = nums2.size();
+        vector<int>ngr(n,-1);
+        
+        map<int,int>mp;
+        
+        for(int i=n-1;i>=0;i--){
+            
+            mp[nums2[i]]=i;
+            if(st.empty()){
+                st.push(nums2[i]);
+                continue;
+            }
+            
+            while(!st.empty()&&st.top()<nums2[i])
+                st.pop();
+            
+            if(!st.empty()){
+                ngr[i]=st.top();
+            }else{
+                ngr[i]=-1;
+            }
+            
+            st.push(nums2[i]);
+            
+            
+        }
+        
+        vector<int>ans;
+        
+        for(int i=0;i<nums1.size();i++){
+            
+            int idx = mp[nums1[i]];
+            ans.push_back(ngr[idx]);
+        }
+        
+        
+        
+        return ans;
+    }
+};
