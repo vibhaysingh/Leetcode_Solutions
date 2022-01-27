b3aeb03d1053bebfc82de4661c732c6e9fccee9b
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-27 14:19:02 +0530

    Time: 32 ms (87.37%), Space: 20.9 MB (95.37%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -1,40 +1,59 @@
 class CustomStack {
 public:
     
-    vector<int>st;
+    vector<int>st,inc;
     int maxSize;
+    
     CustomStack(int maxSize) {
         this->maxSize = maxSize;
-        
-        
     }
     
     void push(int x) {
         
-        if(st.size()<maxSize){
-            st.push_back(x);
-        }
+        if(st.size() == maxSize) return;
+        
+        st.push_back(x);
+        inc.push_back(0);
+        
+        return;
+        
+        
     }
     
     int pop() {
         
-        if(st.empty()) return -1;
+        int i  = st.size()-1;
         
-        int x = st.back();
+        if(i<0) return -1;
+        
+        int incr = inc[i];
+        
+        if(i>0)
+            inc[i-1]+=inc[i];
+        
+       
+        int ans = st.back();
         st.pop_back();
-        return x;
+        inc.pop_back();
+        
+        return ans+incr;
+        
+        
+        
         
     }
     
     void increment(int k, int val) {
         
-        for(int i=0;i<min(k,(int)st.size());i++){
-            
-            st[i]+=val;
-        }
+        // if(st.empty()) return;
+        
+        int i = min(k,(int)st.size())-1;
+        if(i>=0)
+        inc[i]+=val;
         
     }
 };
+
 
 /**
  * Your CustomStack object will be instantiated and called as such:
