f4195c546d15e79dd2d8ac4962085286678556da
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-29 10:29:53 +0530

    Time: 3 ms (90.11%), Space: 6.9 MB (98.74%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -2,28 +2,92 @@ class Solution {
 public:
     string reverseWords(string s) {
         
-        stringstream ss(s);
         
-        string ans;
+        int n = s.size();
         
-        string word;
-        vector<string>v;
-        while(ss>>word){
-            v.push_back(word);
-        }
+        // reverse whole string
         
-        for(int i=v.size()-1;i>=0;i--){
-            
-            ans+=v[i];
-            ans+=" ";
-        }
+        reverse(0,n-1,s);
         
-        ans.pop_back();
+        // reverse each words 
         
-        return ans;
+        reverseWord(s,n);
         
+        /// remove extra spaces
         
+        removeSpaces(s,n);
         
+        return s;
         
     }
+    
+    
+    void reverse(int i,int j,string &s){
+        
+        while(i<j){
+            
+            swap(s[i++],s[j--]);
+        }
+    }
+    
+    void reverseWord(string &s, int n){
+        
+        int i=0;
+        int j=0;
+        
+        while(i<n){
+            
+            i=j;
+            
+            while(i<n && s[i]==' ') i++; // skip spaces
+            
+            // now i points to a character
+            
+            j=i;
+            
+            // increase j untill the next space is found;
+            
+            while(j<n && s[j]!=' ')j++;
+            
+            // j points to space of end of string 
+            
+            reverse(i,j-1,s);            
+        }
+        
+       
+    }
+    
+    void removeSpaces(string &s, int n){
+        
+        int i=0;
+        int j=0;
+        
+        while(j<n){
+            
+            // skip spaces
+            
+            while(j<n && s[j]==' ')j++;
+            
+            // j points to character
+            
+            while(j<n && s[j]!=' '){
+                swap(s[i++],s[j++]);
+            }
+            
+            // j points to space
+            
+            // add one space before prev word
+            
+            if(j<n){
+                swap(s[i++],s[j++]);
+            }
+            
+        }
+        
+         // pop all spaces from back of string
+        
+        while(s.back()==' ') s.pop_back();
+        
+    }
+    
 };
