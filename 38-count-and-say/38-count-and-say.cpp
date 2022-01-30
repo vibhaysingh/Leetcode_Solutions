43cae76b0ee59dd93016c800ebcb4ab962607216
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-30 15:03:30 +0530

    Time: 16 ms (34.87%), Space: 6.8 MB (42.75%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    string solver(string s){
+        
+        int n = s.size();
+        
+        if(n==1){
+            
+           s+=s;
+            return s;
+        }
+        map<char,int>mp;
+        
+      
+        string ans="";
+        int cnt=0;
+        for(int i=0;i<n-1;i++){
+            
+            if(s[i]==s[i+1]) cnt++;
+            
+           else  if(s[i]!=s[i+1]){
+               cnt++;
+                
+                ans+=to_string(cnt)+s[i];
+               cnt=0;
+            }
+        }
+  
+        // 33222111
+        cnt++;
+       ans+=to_string(cnt)+s[n-1];
+        
+        return ans;
+    }
+    
+    string countAndSay(int n) {
+        
+        string temp= "1";
+        if(n==1) return temp;
+        
+        for(int i=2;i<=n;i++){
+            
+            temp = solver(temp);
+            // cout<<temp<<endl;
+            
+        }
+        
+        return temp;
+        
+    }
+};
