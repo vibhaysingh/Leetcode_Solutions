9029665f65586844f2ef136cd17131a46de1de5f
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-01-30 14:22:31 +0530

    Time: 3 ms (74.81%), Space: 7.1 MB (53.20%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        
+        long long ans=0;
+        
+        int m = INT_MAX;
+        int sign = 1;
+        bool f= false;
+        int n = s.size();
+        // cout<<INT_MAX<<endl;
+        // cout<<INT_MIN<<endl;
+        for(int i=0;i<s.size();i++){
+            
+            if(s[i]==' ') continue;
+            
+            if(isdigit(s[i])){
+                
+                
+                
+                ans = (ans*10 + s[i]-'0');
+                
+                if(ans>INT_MAX && sign==1){
+                    ans = INT_MAX;
+                    break;
+                }
+                else if(sign==-1 && (ans*sign)<INT_MIN){
+                    
+                    // cout<<i<<" "<<ans<<endl;
+                    ans = INT_MIN;
+                    
+                    break;
+                }
+                
+                if(i+1<n && !isdigit(s[i+1])) break;
+                
+            }
+            else if(s[i]=='-'){
+                sign=-1;
+                
+                if(i+1<n && !isdigit(s[i+1])) break;
+                 else if(i==n-1)break;
+                else if(i>0 && s[i-1]!=' ')break;
+            }
+            else if(s[i]=='+'){
+                sign=1;
+                if(i+1<n && !isdigit(s[i+1])) break;
+                else if(i==n-1)break;
+                else if(i>0 && s[i-1]!=' ')break;
+                
+            }
+            else{
+                break;
+            }
+            
+            
+        }
+        
+        return sign*ans;
+        
+    }
+};
