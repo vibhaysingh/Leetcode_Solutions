920e2aad220313f71500dd9c76ea51352ad87f04
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-02-27 15:56:55 +0530

    Time: 520 ms (59.09%), Space: 178.8 MB (18.18%) - LeetHub

---
+++ 2188-minimum-time-to-finish-the-race/2188-minimum-time-to-finish-the-race.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minimumFinishTime(vector<vector<int>>& tires, int ct, int nl) {
+        
+        
+        vector<long long >dp1(16,INT_MAX);
+        vector<long long >dp2(nl+1,INT_MAX);
+        
+        int n = tires.size();
+        
+        for(int i=0;i<n;i++){
+            
+            long long  int fi = tires[i][0];
+            long long  int ri = tires[i][1];
+            long long  int ttl = tires[i][0];
+            long long int t = fi;
+            
+            dp1[1] = min(dp1[1],fi);
+            
+            for(int j=2;j<16;j++){
+                
+               t*= ri;
+                
+                ttl+=t;
+                
+                if(ttl>2e5) break;
+                
+                dp1[j] = min(dp1[j],ttl);
+            }
+        }
+        
+        
+        for(int i=1;i<=nl;i++){
+            
+            if(i<16){
+                dp2[i] = dp1[i];
+            }
+            
+            for(int j=i-1;j>=max(i-15,1);j--)
+            {
+                
+                dp2[i] = min(dp2[i],dp2[j]+ct+dp2[i-j]);
+            }
+        }
+        
+        return dp2[nl];
+        
+    }
+};
