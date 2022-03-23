fa5fe8a7542f19514619f60aeb0907c34d76a34c
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-03-23 16:39:33 +0530

    Time: 158 ms (31.25%), Space: 36.5 MB (42.13%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        
+        vector<int>cnt(n,0);
+         map<pair<int,int>,bool>mp;
+        for(auto k:roads){
+            
+            cnt[k[0]]+=1;
+            cnt[k[1]]+=1;
+            int x=k[0],y=k[1];
+                            if(x>y)swap(x,y);
+            mp[{x,y}]=true;
+        }
+        
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(i==j)continue;
+                
+                
+                int x=i,y=j;
+                if(x>y)swap(x,y);
+                
+                if(mp.find(make_pair(x,y))!=mp.end()){
+                    
+                    ans=max(ans,cnt[i]+cnt[j]-1);
+                }
+                else{
+                     ans=max(ans,cnt[i]+cnt[j]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
