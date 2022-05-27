229e982b780dba48fcd2a09f8f4a245623d4f953
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-05-27 15:23:17 +0530

    Time: 12 ms (83.13%), Space: 7.6 MB (93.91%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+        
+        int n = s.size();
+        int m = t.size();
+        
+        int i=0,j=0,count=0;
+        
+     
+        
+        int f[1000]={0};
+        
+        
+        for(int i=0;i<t.size();i++){           
+            // p[t[i]-'A']=1;
+            f[t[i]-'A']++;
+            // mx[t[i]-'A']++;
+        }
+        
+       long long int l=INT_MIN,r=INT_MAX;
+        
+        
+        while(j<n){
+            
+            if( f[s[j]-'A']>0){
+                count++;
+               
+            }
+             f[s[j]-'A']--;
+            
+            if(count==m && (j-i)<(r-l)){
+                 
+                l=i,r=j;
+                
+               
+            }
+            
+            while(i<=j && count==m){
+                
+            if(count==m && (j-i)<(r-l)){
+                
+              
+               
+                l=i,r=j;
+                
+            }
+            
+                f[s[i]-'A']++;
+                if(f[s[i]-'A']>0){
+                    
+                    count--;
+                }
+                i++;
+                
+                
+            }
+
+            j++;
+            
+        }
+        
+        string ans="";
+        
+        if(l==INT_MIN) return ans;
+        
+        for(int i=l;i<=r;i++){
+            ans+=s[i];
+        }
+        
+        return ans;
+    }
+};
