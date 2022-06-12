10fbe3b9987a3f2c21436b6788058414f4d0669c
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-06-12 07:10:19 +0530

    Time: 1266 ms (40.00%), Space: 18.6 MB (40.00%) - LeetHub

---
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool matchReplacement(string s, string sub, vector<vector<char>>& mps) {
+        
+        
+        int n = s.size();
+        int z = sub.size();
+        
+        unordered_map<char,unordered_set<char>>mp;
+        
+        for(auto k:mps){
+            
+            mp[k[0]].insert(k[1]);
+        }
+        
+       
+        
+        for(int i=0;i<=n-z;i++)  {
+            
+            
+                
+                int j=0;
+                int l = i;
+                
+                
+                
+        while(j<sub.size() && l<s.size() && ( s[l]==sub[j]  ||  (  mp.find(sub[j])!=mp.end()  && mp[sub[j]].find(s[l]) !=mp[sub[j]].end()) ))             {                  
+            
+                    l++;
+                    j++;
+            
+                
+                }
+            
+          
+                
+
+                      
+                if(j==sub.size()) return true;
+            
+          
+                
+            }
+        
+            return false;
+        
+        }
+    
+                
+                    
+            
+        
+
+};
