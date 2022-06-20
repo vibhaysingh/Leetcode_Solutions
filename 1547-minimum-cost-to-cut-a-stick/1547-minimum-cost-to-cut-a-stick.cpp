7e33d6c9e9f4c62f97bdabf419713def2dee68ee
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-06-20 14:30:59 +0530

    Time: 49 ms (95.93%), Space: 8.5 MB (87.92%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -4,35 +4,50 @@ int dp[105][105];
 class Solution {
 public:
     
-    int solver(int i,int j, vector<int>&cuts){
+//     int solver(int i,int j, vector<int>&cuts){
         
-        if(i>j){
-            return 0;
-        }
+//         if(i>j){
+//             return 0;
+//         }
         
-        // cout<<i<<" "<<j<<endl;
+//         // cout<<i<<" "<<j<<endl;
         
-        if(dp[i][j]!=-1) return dp[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        int temp=INT_MAX;
+//         int temp=INT_MAX;
         
-        for(int k=i;k<=j;k++){
+//         for(int k=i;k<=j;k++){
             
-             int s = (cuts[j+1]-cuts[i-1]) + solver(i,k-1,cuts) + solver(k+1,j,cuts);
-            temp=min(temp,s);
-        }
+//              int s = (cuts[j+1]-cuts[i-1]) + solver(i,k-1,cuts) + solver(k+1,j,cuts);
+//             temp=min(temp,s);
+//         }
         
-        return dp[i][j]=temp;
-    }
+//         return dp[i][j]=temp;
+//     }
     
     
     int minCost(int n, vector<int>& cuts) {
         int l= cuts.size();
-        memset(dp,-1,sizeof dp);
+        memset(dp,0,sizeof dp);
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
         
-        return solver(1,l,cuts);
+        for(int i=l;i>=1;i--){
+            for(int j=1;j<=l;j++){
+                
+                if(i>j)continue;
+                int temp=INT_MAX;
+        
+        for(int k=i;k<=j;k++){
+            
+             int s = (cuts[j+1]-cuts[i-1]) + dp[i][k-1] + dp[k+1][j];
+            temp=min(temp,s);
+        }        
+        dp[i][j]=temp;
+        }
+    }
+        
+        return dp[1][l];
     }
 };
