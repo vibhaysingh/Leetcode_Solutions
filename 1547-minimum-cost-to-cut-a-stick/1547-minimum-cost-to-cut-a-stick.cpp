706d2f4472e83408df095226b86cc4543866621f
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-06-20 14:23:54 +0530

    Time: 57 ms (89.40%), Space: 8.4 MB (87.92%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,38 @@
+
+int dp[105][105];
+
+class Solution {
+public:
+    
+    int solver(int i,int j, vector<int>&cuts){
+        
+        if(i>j){
+            return 0;
+        }
+        
+        // cout<<i<<" "<<j<<endl;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int temp=INT_MAX;
+        
+        for(int k=i;k<=j;k++){
+            
+             int s = (cuts[j+1]-cuts[i-1]) + solver(i,k-1,cuts) + solver(k+1,j,cuts);
+            temp=min(temp,s);
+        }
+        
+        return dp[i][j]=temp;
+    }
+    
+    
+    int minCost(int n, vector<int>& cuts) {
+        int l= cuts.size();
+        memset(dp,-1,sizeof dp);
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        
+        return solver(1,l,cuts);
+    }
+};
