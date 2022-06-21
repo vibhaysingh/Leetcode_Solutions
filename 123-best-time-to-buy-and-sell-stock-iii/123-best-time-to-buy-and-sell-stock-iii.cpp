889ded1968a6dd5621781abc5723785bb8e37261
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-06-21 10:14:50 +0530

    Time: 273 ms (52.92%), Space: 77.7 MB (66.44%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,41 +5,72 @@ class Solution {
 public:
     
     
-    int solver(int idx,int k,int own,vector<int>&prices){
+//     int solver(int idx,int k,int own,vector<int>&prices){
+        
+//         int n = prices.size();
+//         if(idx==n||k==0) return 0;
+        
+        
+//         if(dp[idx][own][k]!=-1) return dp[idx][own][k];
+        
+//         // if i have stock
+        
+        
+        
+//         if(own){
+            
+//             // sell and not sell
+            
+//             int op1 = prices[idx] + solver(idx+1,k-1,0,prices);
+//             int op2 = 0+ solver(idx+1,k,1,prices);
+            
+//             return dp[idx][own][k]=max(op1,op2);
+//         }
+//         else{
+            
+//             // buy & not buy
+            
+//               int op1 = -prices[idx] + solver(idx+1,k,1,prices);
+//               int op2 = 0 + solver(idx+1,k,0,prices);
+//             return dp[idx][own][k]=max(op1,op2);
+//         }
+//     }
+    
+    int maxProfit(vector<int>& prices) {
+        
+        memset(dp,0,sizeof dp);
         
         int n = prices.size();
-        if(idx==n||k==0) return 0;
         
-        
-        if(dp[idx][own][k]!=-1) return dp[idx][own][k];
-        
-        // if i have stock
-        
-        
-        
+        for(int idx=n-1;idx>=0;idx--){
+            
+            for(int k=1;k<=2;k++)
+            {
+                for(int own = 0;own<=1;own++){
+                    
+                      
         if(own){
             
             // sell and not sell
             
-            int op1 = prices[idx] + solver(idx+1,k-1,0,prices);
-            int op2 = 0+ solver(idx+1,k,1,prices);
+            int op1 = prices[idx] +  dp[idx+1][0][k-1];
+            int op2 = 0+ dp[idx+1][1][k];
             
-            return dp[idx][own][k]=max(op1,op2);
+             dp[idx][own][k]=max(op1,op2);
         }
         else{
             
             // buy & not buy
             
-              int op1 = -prices[idx] + solver(idx+1,k,1,prices);
-              int op2 = 0 + solver(idx+1,k,0,prices);
-            return dp[idx][own][k]=max(op1,op2);
+              int op1 = -prices[idx] + dp[idx+1][1][k];
+              int op2 = 0 + dp[idx+1][0][k];
+             dp[idx][own][k]=max(op1,op2);
         }
-    }
-    
-    int maxProfit(vector<int>& prices) {
+                }
+            }
+        }
         
-        memset(dp,-1,sizeof dp);
         
-        return solver(0,2,0,prices);
+        return dp[0][0][2];
     }
 };
