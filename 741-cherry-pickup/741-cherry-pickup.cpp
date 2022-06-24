f2cfc27c3a90dc7f6514f67fd56661928db6add8
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-06-24 12:51:51 +0530

    Time: 46 ms (93.14%), Space: 9.6 MB (85.33%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -1,20 +1,23 @@
 
-int dp[52][52][52][52];
+int dp[52][52][52];
 
 class Solution {
 public:
     
+    // r1+c1 == r2+c2 (always)
+    // c2 = (r1+c1)-r2;
     
-    int solver(int r1,int c1,int r2,int c2,vector<vector<int>>&grid){
+    int solver(int r1,int c1,int r2,vector<vector<int>>&grid){
         
         int n = grid.size();
+        int c2 = (r1+c1)-r2;
 
          if(r1>=n||r2>=n ||c1>=n||c2>=n || grid[r1][c1]==-1||grid[r2][c2]==-1) return INT_MIN;
         
         if(r1==n-1 && c1==n-1) return grid[r1][c1];
         
-          if( dp[r1][c1][r2][c2]!= -1) 
-             return  dp[r1][c1][r2][c2];
+          if( dp[r1][c1][r2]!= -1) 
+             return  dp[r1][c1][r2];
         
         int ans=0;
         
@@ -29,14 +32,14 @@ public:
         
       
         
-        int br = solver(r1,c1+1,r2,c2+1,grid);
-        int bd = solver(r1+1,c1,r2+1,c2,grid);
-        int orod = solver(r1,c1+1,r2+1,c2,grid);
-        int odor = solver(r1+1,c1,r2,c2+1,grid);
+        int br = solver(r1,c1+1,r2,grid);
+        int bd = solver(r1+1,c1,r2+1,grid);
+        int orod = solver(r1,c1+1,r2+1,grid);
+        int odor = solver(r1+1,c1,r2,grid);
         
         ans  = ans+  max({br,bd,orod,odor});
         
-        return dp[r1][c1][r2][c2]=ans;
+        return dp[r1][c1][r2]=ans;
     }
     
     
@@ -44,6 +47,6 @@ public:
         
         memset(dp,-1,sizeof dp);
         
-        return max(0,solver(0,0,0,0,grid));
+        return max(0,solver(0,0,0,grid));
     }
 };
