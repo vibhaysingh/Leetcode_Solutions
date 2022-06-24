94520f7f46b21f8d21c17efd3b3088afc0b753c9
Vibhay Singh <76516741+vibhaysingh@users.noreply.github.com>
2022-06-24 16:42:30 +0530

    Time: 173 ms (60.29%), Space: 29.3 MB (63.59%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -1,6 +1,21 @@
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+ 
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;             
+        }
+         
+        // If hash1 == hash2, their XOR is zero.
+          return hash1;
+    }
+};
 class Solution {
 public:
-map<pair<string,string>,bool> mp;          //Create Map Globally
+unordered_map<pair<string,string>,bool,hash_pair> mp;          //Create Map Globally
     bool isScramble(string s1, string s2) {
         
         int n = s1.size();
